582600a6c77491dc3bbb1152df3d400023dbe94b
zhoushuqun <zhoushuqun@313a52ac-f333-0410-96e4-4b8b98d1e50a>
2007-06-28 10:19:34 +0000

    Import the initial project.

---
+++ trunk/src/main/java/com/redv/jdigg/domain/User.java
@@ -0,0 +1,223 @@
+/**
+ * 
+ */
+package com.redv.jdigg.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @author <a href="mailto:zhoushuqun@gmail.com">Sutra Zhou</a>
+ * 
+ */
+public class User implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8178507006598140894L;
+
+	private String id;
+
+	private String openid;
+
+	private Date joinDate;
+
+	private String nickname;
+
+	private String email;
+
+	private String fullname;
+
+	private Date dob;
+
+	private String gender;
+
+	private String postcode;
+
+	private String country;
+
+	private String language;
+
+	private String timezone;
+
+	/**
+	 * @return the joinDate
+	 */
+	public Date getJoinDate() {
+		return joinDate;
+	}
+
+	/**
+	 * @param joinDate
+	 *            the joinDate to set
+	 */
+	public void setJoinDate(Date joinDate) {
+		this.joinDate = joinDate;
+	}
+
+	/**
+	 * @return the country
+	 */
+	public String getCountry() {
+		return country;
+	}
+
+	/**
+	 * @param country
+	 *            the country to set
+	 */
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	/**
+	 * @return the dob
+	 */
+	public Date getDob() {
+		return dob;
+	}
+
+	/**
+	 * @param dob
+	 *            the dob to set
+	 */
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email
+	 *            the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return the fullname
+	 */
+	public String getFullname() {
+		return fullname;
+	}
+
+	/**
+	 * @param fullname
+	 *            the fullname to set
+	 */
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+
+	/**
+	 * @return the gender
+	 */
+	public String getGender() {
+		return gender;
+	}
+
+	/**
+	 * @param gender
+	 *            the gender to set
+	 */
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the language
+	 */
+	public String getLanguage() {
+		return language;
+	}
+
+	/**
+	 * @param language
+	 *            the language to set
+	 */
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+
+	/**
+	 * @return the nickname
+	 */
+	public String getNickname() {
+		return nickname;
+	}
+
+	/**
+	 * @param nickname
+	 *            the nickname to set
+	 */
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	/**
+	 * @return the openid
+	 */
+	public String getOpenid() {
+		return openid;
+	}
+
+	/**
+	 * @param openid
+	 *            the openid to set
+	 */
+	public void setOpenid(String openid) {
+		this.openid = openid;
+	}
+
+	/**
+	 * @return the postcode
+	 */
+	public String getPostcode() {
+		return postcode;
+	}
+
+	/**
+	 * @param postcode
+	 *            the postcode to set
+	 */
+	public void setPostcode(String postcode) {
+		this.postcode = postcode;
+	}
+
+	/**
+	 * @return the timezone
+	 */
+	public String getTimezone() {
+		return timezone;
+	}
+
+	/**
+	 * @param timezone
+	 *            the timezone to set
+	 */
+	public void setTimezone(String timezone) {
+		this.timezone = timezone;
+	}
+
+}
