bc3b9e802de6c5611fc49bcd56fd28eb175ce014
zhoushuqun <zhoushuqun@313a52ac-f333-0410-96e4-4b8b98d1e50a>
2007-06-30 06:06:38 +0000

    Install dwr.

---
+++ trunk/src/main/java/com/redv/jdigg/web/dwr/Digg.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.redv.jdigg.web.dwr;
+
+import com.redv.jdigg.domain.Story;
+import com.redv.jdigg.service.DiggService;
+
+/**
+ * @author <a href="mailto:zhoushuqun@gmail.com">Sutra Zhou</a>
+ * 
+ */
+public class Digg {
+	private DiggService diggService;
+
+	/**
+	 * @param diggService
+	 *            要设置的 diggService
+	 */
+	public void setDiggService(DiggService diggService) {
+		this.diggService = diggService;
+	}
+
+	public Story getStory(String id) {
+		return diggService.getStory(id);
+	}
+}
